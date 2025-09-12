cb7a671a97b14d53519bd68fcbc36a161a08b87b
Yulya <102688044+FoxYulya@users.noreply.github.com>
2025-09-12 13:03:56 +0300

    Update OrdersDetails.tsx

--- flower-delivery-frontend/src/components/OrdersDetails.tsx
+++ flower-delivery-frontend/src/components/OrdersDetails.tsx
@@ -3,7 +3,7 @@ import { useParams, Link } from "react-router-dom";
 import axios from "axios";
 import type { OrderDetails } from "../types";
 
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = "https://flowerddelivery-4.onrender.com/api";
 
 const OrdersDetails: React.FC = () => {
   const { id } = useParams<{ id: string }>();
