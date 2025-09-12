49e62848cdebd4a8d56b3c4b2eeafa357280bcc3
Yulya <Yulya_Nosova123@ukr.net>
2025-09-12 12:58:05 +0300

    Update API

--- flower-delivery-frontend/src/components/ShopList.tsx
+++ flower-delivery-frontend/src/components/ShopList.tsx
@@ -4,7 +4,7 @@ import axios from "axios";
 import type { Shop } from "../types";
 import { useAnchorScroll } from "../hooks/useAnchorScroll";
 
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = "https://flowerddelivery-4.onrender.com/api";
 
 const flowerImages = ["/images/1.JPG", "/images/2.JPG", "/images/3.JPG"];
 
