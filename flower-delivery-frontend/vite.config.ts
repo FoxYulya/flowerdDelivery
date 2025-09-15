1dc8ce67d9b2291653f9faaec1b22b5d96664547
Yulya <Yulya_Nosova123@ukr.net>
2025-09-15 11:00:30 +0300

    fix: addl

--- flower-delivery-frontend/vite.config.ts
+++ flower-delivery-frontend/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/flowerdDelivery/', // Важно: название вашего репозитория
+  base: '/', // Для Netlify используйте корневой путь
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
